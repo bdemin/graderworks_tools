e5ae7a41ce7cd972b9ec62b846b7e0a1414d873a
bdemin <42113340+bdemin@users.noreply.github.com>
2020-07-02 16:23:07 +0300

    Keep folders inside a dictionary rather than a list

--- move_images.py
+++ move_images.py
@@ -1,6 +1,6 @@
 def separate_files(files):
     images = []
-    folders = []
+    folders = dict()
 
     for f_name in files:
         f_split = f_name.split('.')
@@ -9,8 +9,8 @@ def separate_files(files):
         if f_ext == 'jpg':
             images.append(f_name)
         elif f_split[-1] == f_name:
+            student_name = f_name.split('_')[0]
             folders[student_name.lower()] = f_name
-            folders.append(f_name)
         else:
             print('Ignoring', f_name)
     return images, folders
